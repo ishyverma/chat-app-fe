ed16e2f47b25f70e17e5d976ccd06e0fee96d5a5
ishyverma <ishyamver@gmail.com>
2024-12-04 23:27:12 +0530

    done basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { useEffect, useRef, useState } from "react"
 
+interface MessageType {
+  isSendByMe: boolean;
+  message: string;
+}
+
 function App() {
   const websocket = useRef<WebSocket>()
-  const [otherMessage, setOtherMessage] = useState<string[]>([])
   const messageRef = useRef<HTMLInputElement | null>(null)
-  const [myMessage, setMyMessage] = useState<string[]>([])
+  const [myMessage, setMyMessage] = useState<MessageType[]>([])
 
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:8080")
     ws.onmessage = (message) => {
-      setOtherMessage(prev => [...prev, message.data])
+      setMyMessage(prev => [...prev, { isSendByMe: false, message: message.data }])
     }
 
     ws.onopen = () => {
@@ -31,7 +35,7 @@ function App() {
   function sendMessage() {
     const message = messageRef.current?.value
     if (message?.trim()) {
-      setMyMessage(prev => [...prev, message])
+      setMyMessage(prev => [...prev, { isSendByMe: true, message }])
       websocket.current?.send(JSON.stringify({
         type: "chat",
         roomId: "red",
@@ -45,17 +49,10 @@ function App() {
   return <div>
     <input ref={messageRef} type="text" placeholder="Send message" />
     <button onClick={sendMessage}>Send</button>
-    <div className={`flex justify-between`}>
-        <div>
-        {otherMessage.map((items, index) => <div className="bg-gray-400" key={index}>
-          {items}
-        </div>)}
-      </div>
-      <div>  
-        {myMessage.map((items, index) => <div className="bg-green-400" key={index}>
-          {items}
-        </div>)}
-      </div>
+    <div className={`flex flex-col`}>
+        {myMessage.map(({ isSendByMe, message }, index) => 
+        <div className={`${isSendByMe ? "self-end bg-green-400" : "self-start bg-gray-400"}`} key={index}>{message}</div>
+        )}
     </div>
   </div>
 }
